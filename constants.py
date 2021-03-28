d9d01d575bb5960a58d15e6849d60940a0ca7534
Chenxi Qiu <c.qiu914@gmail.com>
2021-03-28 15:29:08 +0200

    Create constants.py
    extra module for only constants

---
+++ constants.py
@@ -0,0 +1,15 @@
+#age spectra
+VMIN = 0 #the lowest possible value for age variables
+VMAX = 100 #the highest possible value for age variables except mean age or medium age
+VMAX_AGE = 80 #the lowest possible value for mean age or medium age
+VRES = 5 #the size of the bins for age variables
+#OCS
+OCSMIN_MIPAS = -200
+OCSMIN_ACE = -60
+OCSMAX = 600
+OCSRES = 20
+#N2O
+N2OMIN_MIPAS = -200
+N2OMAX_ACE = -60
+N2OMAX = 600
+N2ORES = 20
