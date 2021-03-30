077a99e6e0fa83c802e3242b2ce67b9ec4d46f97
Chenxi Qiu <c.qiu914@gmail.com>
2021-03-30 10:28:58 +0200

    Update f0022_OCS_latitude_bands_local_flights.py

--- f0022_OCS_latitude_bands_local_flights.py
+++ f0022_OCS_latitude_bands_local_flights.py
@@ -29,100 +29,84 @@ pio.renderers.default='browser'
 
 
 def group(df):
+    df = df[['ALT', 'LAT', 'OCS']]
     alt_range = np.arange(9, 14+0.5, 0.5) 
     lat_range = [-70, -60, -50, -35]#np.arange(-70, -35+10, 10) 
-    output = df['OCS'].groupby([pd.cut(df['ALT'], alt_range), pd.cut(df['LAT'], lat_range)]).describe().reset_index() #apply(get_stats) .unstack()
+    output = df['OCS'].groupby([pd.cut(df['ALT'], alt_range), pd.cut(df['LAT'], lat_range)]).describe() #apply(get_stats) .unstack()
     print(output.head())
-    output['alt'] = output.apply(lambda x: x['ALT'].left+0.5/2,axis=1)
-    output['lat'] = output.apply(lambda x: x['LAT'].left+5/2,axis=1)
-    return output  
+    return output
 
-# amica 
-df = southtrac.read(strat=0, local=1)
+df = southtrac.read(strat=1, local=1)
+plot_data_p1 = group(df[(df.index.month==9) | (df.index.month==10)])
+plot_data_p2 = group(df[(df.index.month==11)])
+plot_data_dif = plot_data_p1['mean'] - plot_data_p2['mean']
 
-def plot(label=None, df=None, ax=None, shift=0, **kwargs):
+def plotting(label=None, df=None, ax=None, shift=0, **kwargs):
     if ax is None:
         ax = plt.gca()   
     ax.errorbar(x=df['mean'], 
-                y=df['alt']+shift, 
+                y=[(x.left+x.right)/2+shift for x in group['mean'].index.get_level_values('ALT')],
                 xerr=df['std'],
                 capsize=4,
-                label=label,
                 ecolor='#BDBDBD',
                 markeredgecolor='dimgrey',
+                label=str(key),
                 **kwargs
                 )
+    ax.grid()
     return ax
 
-toplot_p1 = group(df[(df.index.month==9) | (df.index.month==10)]).groupby('LAT')
-toplot_p2 = group(df[df.index.month==11]).groupby('LAT')
+color_range = ('dodgerblue', 'forestgreen', 'darkorchid')
+fmt = ('o', "v", "s")
 
-color_range = ['dodgerblue', 'forestgreen']
-fmt = ['o', "v", "s"]
+fig = plt.figure(figsize=(10, 50))
+spec = gridspec.GridSpec(nrows=1, ncols=3, width_ratios=[1, 1, 0.5])#,height_ratios=[15,1]) width_ratios=[9,1]
 
-fig, ax = plt.subplots()
-
-for i, (key, _) in enumerate(toplot_p1): 
-    plot(str(key)+' phase 1',
-     toplot_p1.get_group(key),
-     shift=-(i+1)/30,
-     color=color_range[0],
-     fmt=fmt[i],
-     )
-    
-for i, (key, _) in enumerate(toplot_p2): 
-    plot(str(key)+' phase 2',
-     toplot_p2.get_group(key),
-     shift=(i+1)/30,
-     color=color_range[1],
-     fmt=fmt[i],
-     )
-
-ax.set(
+ax1 = fig.add_subplot(spec[0, 0])
+grouped_p1 = plot_data_p1.groupby('LAT')
+for i, (key, group) in enumerate(grouped_p1):
+    plotting(
+        df=group,
+        ax=ax1,
+        shift=(i-1)/50,
+        color=color_range[i],
+        fmt='-o'
+        )
+ax1.legend()
+ax1.set(
    xlabel ='OCS / ppt', 
    ylabel ='altitude / km', 
    xlim =(150, 550), 
    ylim =(9, 14), 
-   title='alt>9km'
+   title='phase 1'
    ) 
 
-ax.grid()
-plt.legend()   
-    
-# plot('local flights: phase 1',
-#      df[(df.index.month==9) | (df.index.month==10)],
-#      shift=0,
-#      color='DarkBlue',
-#      ecolor='LightBlue',
-#      )
+ax2 = fig.add_subplot(spec[0, 1], sharex=ax1, sharey=ax1)
+grouped_p2 = plot_data_p2.groupby('LAT')
+for i, (key, group) in enumerate(grouped_p2):
+    plotting(
+        df=group,
+        ax=ax2,
+        shift=(i-1)/50,
+        color=color_range[i],
+        fmt='-o'
+        )
+ax2.set_xlabel('OCS / ppt')
+ax2.set_title('phase 2')
 
-# plot('local flights: phase 2',
-#      df[df.index.month==11],
-#      shift=0.05,
-#      color='darkmagenta',
-#      ecolor='violet',
-#      )
+ax3 = fig.add_subplot(spec[0, 2], sharey=ax1)
+grouped_dif = plot_data_dif.groupby('LAT')
+for i, (key, group) in enumerate(grouped_dif):
+    ax3.plot(
+        group.values,
+        [(i.left+i.right)/2 for i in group.index.get_level_values('ALT')],
+        color=color_range[i],
+        label=str(key),
+        marker='s'
+        )
+ax3.set_xlabel('difference in OCS / ppt')
+ax3.set_xlim(-100, 100)
+ax3.set_title('difference 1-2')
+ax3.grid()   
 
-# plt.legend()
-
-# ax=groups_sep.reset_index().plot(kind = "scatter", 
-#                                  x='lat', y='mean',
-#                                  yerr = "SD", capsize=10,#,capthick=1,
-#                                  legend = 'SEP', 
-#                                  title = "Average Avocado Prices",
-#                                  ax=ax)
-
-# scatter(x='lat', y='mean', color='DarkBlue', label='SEP',s=80)
-# groups_oct.reset_index().plot.scatter(x='lat', y='mean', color='DarkGreen', label='OCT', s=80,
-#                                       ax=ax)
-
-# means = pd.concat([groups_sep['mean'], groups_oct['mean'],groups_sep['mean']-groups_oct['mean']], 
-#                    axis='columns') 
-# means.columns =['sep','oct','dif']
-
-# means.plot.scatter()
-
-
-# lat_res=5
-# lat_range = np.arange(-90,90+lat_res,lat_res) 
-# df_sep['AMICA_OCS'].groupby([pd.cut(df_sep.IRS_LAT, lat_range)]).plot.box()
+plt.show()
