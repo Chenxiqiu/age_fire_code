1b535b007ade3e6fd000c726541eb111b67ced53
Chenxi Qiu <c.qiu914@gmail.com>
2021-03-31 09:58:31 +0200

    Update f0022_OCS_latitude_bands_local_flights.py

--- f0022_OCS_latitude_bands_local_flights.py
+++ f0022_OCS_latitude_bands_local_flights.py
@@ -19,7 +19,7 @@ pio.renderers.default='browser'
 def group(df):
     df = df[['ALT', 'LAT', 'OCS']]
     alt_range = np.arange(9, 14+0.5, 0.5) 
-    lat_range = [-70, -60, -50, -35]
+    lat_range = [-70, -60, -50, -40]
     output = df['OCS'].groupby([pd.cut(df['ALT'], alt_range), pd.cut(df['LAT'], lat_range)]).agg(['mean', 'std'])
     return output
 
@@ -28,7 +28,15 @@ plot_data_p1 = group(df[(df.index.month==9) | (df.index.month==10)])
 plot_data_p2 = group(df[(df.index.month==11)])
 plot_data_dif = plot_data_p1['mean'] - plot_data_p2['mean']
 
-
+index = [pd.Interval(-70, -60, closed='right'),
+         pd.Interval(-60, -50, closed='right'),
+         pd.Interval(-50, -40, closed='right'),]
+tro_hs_v = [
+    dict(sep = 10.4, nov = 9.1), 
+    dict(sep = 9.8, nov = 9.5), 
+    dict(sep = 10.1, nov = 10.6), 
+    ]
+trp_hs = dict(zip(index, tro_hs_v))
 
 def plotting(label=None, df=None, ax=None, shift=0, **kwargs):
     if ax is None:
@@ -45,7 +53,8 @@ def plotting(label=None, df=None, ax=None, shift=0, **kwargs):
     ax.grid()
     return ax
 
-color_range = ('dodgerblue', 'darkorange', 'forestgreen')
+colors_v = ('dodgerblue', 'darkorange', 'forestgreen')
+colors = dict(zip(index, colors_v))
 
 fig = plt.figure(figsize=(10, 50))
 font = {'size': 20}
@@ -60,10 +69,11 @@ for i, (key, group) in enumerate(grouped_p1):
         df=group,
         ax=ax1,
         shift=(i-1)/50,
-        color=color_range[i],
-        ecolor=color_range[i],
+        color=colors[key],
         fmt='-o',
         )
+    trp_h = trp_hs[key]['sep']
+    ax1.plot([0, 1000], [trp_h]*2, color=colors[key], ls='--')
 ax1.legend()
 ax1.set(
    xlabel ='OCS / ppt', 
@@ -80,9 +90,11 @@ for i, (key, group) in enumerate(grouped_p2):
         df=group,
         ax=ax2,
         shift=(i-1)/50,
-        color=color_range[i],
+        color=colors[key],
         fmt='-o'
         )
+    trp_h = trp_hs[key]['nov']
+    ax2.plot([0, 1000], [trp_h]*2, color=colors[key], ls='--')
 ax2.set_xlabel('OCS / ppt')
 ax2.set_title('phase 2')
 
@@ -92,7 +104,7 @@ for i, (key, group) in enumerate(grouped_dif):
     ax3.plot(
         group.values,
         [(i.left+i.right)/2 for i in group.index.get_level_values('ALT')],
-        color=color_range[i],
+        color=colors[key],
         label=str(key),
         marker='s',
         markeredgecolor='dimgrey',
