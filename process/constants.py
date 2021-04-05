778bb521f46561a16b79d189b97e75243dc203e2
Chenxi Qiu <c.qiu914@gmail.com>
2021-04-05 14:38:57 +0200

    Update constants.py

--- process/constants.py
+++ process/constants.py
@@ -7,6 +7,9 @@ VMAX_AGE = 80 #the lowest possible value for mean age or medium age
 VRES = 5 #the size of the bins for age variables
 VRANGE = np.arange(VMIN, VMAX+VRES, VRES)
 VRANGE_AGE = np.arange(VMIN, VMAX_AGE+VRES, VRES)
+ALL_AGEV = ('AGE', 'MF_03', 'MF_06', 'MF_12', 'MF_24', 'MF_48')
+ALL_AGEV_SOUTHTRAC = ('P50', 'MF_06', 'MF_24', 'AGE')
+
 #OCS
 OCSMIN_MIPAS = -200
 OCSMIN_ACE = -60
@@ -14,6 +17,8 @@ OCSMAX = 600
 OCSRES = 20
 OCSRANGE_MIPAS = np.arange(OCSMIN_MIPAS, OCSMAX+OCSRES, OCSRES)
 OCSRANGE_ACE = np.arange(OCSMIN_ACE, OCSMAX+OCSRES, OCSRES)
+OCSRANGE_SOUTHTRAC = np.arange(0, OCSMAX+OCSRES, OCSRES)
+
 #N2O
 N2OMIN_MIPAS = -200
 N2OMIN_ACE = -60
